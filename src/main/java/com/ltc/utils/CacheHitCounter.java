52349031a5a8f5502855aaf7da07044b010f6604
liutongchun <17681102655@163.com>
2020-03-18 14:15:59 +0800

    redis工具类

---
+++ src/main/java/com/ltc/utils/CacheHitCounter.java
@@ -0,0 +1,46 @@
+package com.ltc.utils;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
+/**
+ * @Auther: ltc
+ * @Date: 2020/3/18 14:13
+ * @Description: 
+ */
+public class CacheHitCounter {
+
+    private static final AtomicLongFieldUpdater<CacheHitCounter> hit_count_updater = AtomicLongFieldUpdater.newUpdater(CacheHitCounter.class, "hitCount");
+    private static final CopyOnWriteArrayList<CacheHitCounter> hitCounters = new CopyOnWriteArrayList<>();
+
+    private Object key;
+    private volatile long hitCount;
+
+    public CacheHitCounter(Object key, long hitCount) {
+        this.key = key;
+        this.hitCount = hitCount;
+    }
+
+    public static long getHitCount(Object key) {
+        return getHitCounter(key).hitCount;
+    }
+
+    public static long incrementAndGet(Object key) {
+        return hit_count_updater.incrementAndGet(getHitCounter(key));
+    }
+
+    public static void reset(Object key) {
+        hit_count_updater.set(getHitCounter(key), 0L);
+    }
+
+    private static CacheHitCounter getHitCounter(Object key) {
+        CacheHitCounter hitCounter;
+        if (hitCounters.stream().filter(i -> i.key.equals(key)).count() > 0L) {
+            hitCounter = hitCounters.stream().filter(i -> i.key.equals(key)).findFirst().get();
+        } else {
+            hitCounter = new CacheHitCounter(key, 0L);
+            hitCounters.add(hitCounter);
+        }
+        return hitCounter;
+    }
+}
