ceaf49e974fbd827c4af7f314f9fa8f3b8878383
liutongchun <17681102655@163.com>
2020-03-18 16:28:33 +0800

    修改

---
+++ src/main/java/com/ltc/config/RedisConfigs.java
@@ -0,0 +1,49 @@
+package com.ltc.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+
+/**
+ * @Auther: ltc
+ * @Date: 2020/3/18 16:25
+ * @Description: Redis配置
+ */
+@Configuration
+public class RedisConfigs extends CachingConfigurerSupport {
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(connectionFactory);
+        stringRedisTemplate.afterPropertiesSet();
+        return stringRedisTemplate;
+    }
+
+    @Bean
+    public HashOperations<String, String, String> hashOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    @Bean
+    public ValueOperations<String, String> valueOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    @Bean
+    public ListOperations<String, String> listOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    @Bean
+    public SetOperations<String, String> setOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    @Bean
+    public ZSetOperations<String, String> zSetOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+}
--- src/test/java/com/ltc/Redis_Set_Test.java
+++ src/test/java/com/ltc/Redis_Set_Test.java
@@ -1,15 +1,18 @@
 package com.ltc;
 
 import com.ltc.utils.RedisUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author : ltc
  * @date : 2020/3/17 20:09
  * @description :
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Redis_Set_Test {
 
